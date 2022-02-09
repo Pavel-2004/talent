26f2817966e80570dbe8b276a879cccbc8cbc2ac
Danylo <danylogera376@gmail.com>
2022-02-09 21:59:38 +0200

    design-correcting

---
+++ public/overview.js
@@ -0,0 +1,17 @@
+
+createBlackText("blackText", "TALENT ASSEESSMENT")
+createOrangeText("orangeText", "ЯКІ У ТЕБЕ ТАЛАНТИ?")
+createBlackText("smallText", "МЕТОДИКИ")
+createSmallText("titleText", "§w")
+optionSlider("slider", 100, 10)
+createSmallText("titleText2", "§1. Генетичний/ Природний потенціал")
+optionSlider("slider2", 100, 0)
+
+
+nav("navBar", ["faq", "overview", "results", "profile"], "Methods")
+//otherCircle
+
+
+function redirect(){
+    window.location.href = "overview-specific"
+}
